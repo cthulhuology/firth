ba2f814325237d8f8d0e8058d0f54847832f6d34
Dave Goehrig <dgoehrig@critical-media.com>
2012-07-15 12:08:01 -0400

    moving octal and hexidecimal to tools.asm

--- term.asm
+++ term.asm
@@ -11,14 +11,7 @@ term:
 	key
 	call test_quit
 	call test_retn
-	dupe
 	type
-	call space
-	dupe
-	call octal
-	call space
-	call hexen
-	call space
 	jmp term
 
 test_quit:
@@ -75,53 +68,6 @@ clear:
 	literal 99
 	type
 	ret
-
-octal:		
-	dupe		; a -- a a
-	shiftrnum 3	; a a -- a a/8
-	dupe		; a -- a a/8 a/8
-	shiftrnum 3	; a -- a a/8 a/16
-	andnum 7	; 
-	addnum 48	;
-	type		; a/16&7
-	andnum 7	;
-	addnum 48	;
-	type		; a/8&7
-	andnum 7	;
-	addnum 48	;
-	type		; a&7
-	ret
-
-hexen:
-	dupe
-	shiftrnum 4		; a - a a/16
-	andnum 15		; a - a a/16&15
-	literal 9		; a - a a/16&15 9
-	more			; a - a a/16&15 flag
-	method first_digit	; a - a a/16&15 flag digit
-	if			; a - a a/16&15
-hexen_cont:
-	addnum 48		; "a"-10
-	type
-	andnum 15
-	literal 9
-	more
-	method second_digit
-	if
-	addnum 48
-	type
-	ret
-
-first_digit:
-	addnum 39
-	method hexen_cont
-	invoke
-
-second_digit:
-	addnum 87
-	type
-	ret
-
 
 ; ANSI color codes and jazz
 align 8
