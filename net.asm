e540d63e008c2c5d4791e2cbc366c06c23e17035
David Goehrig <dave@dloh.org>
2013-12-03 20:49:31 -0500

    Adding more net routines, now should be a decent server

--- net.asm
+++ net.asm
@@ -50,12 +50,99 @@
 	os
 %endmacro
 
-%macro connect 0
+%macro connect_socket 0
 	literal 16		; sizeof sockaddr
 	arg3
-	arg2			; sockaddr nos
-	arg1			; fd tos
+	arg2			; sockaddr tos
+	arg1			; fd nos
 	literal connect		
 	os
 %endmacro
 
+%macro send_to_socket 0
+	literal 16
+	arg6	
+	arg5			; sockaddr tos
+	literal 0
+	arg4			; no flags
+	arg3			; length
+	arg2			; buffer
+	arg1 			; fd	
+	literal sendto
+	os
+%endmacro
+
+%macro recv_from_socket 0
+	jmp .skip
+	literal 0		; null addrlen
+	arg6
+	literal 0		; null addr
+	arg5 
+	literal 0		; no flags
+	arg4	
+	arg3			; length tos
+	arg2			; buffer nos
+	arg1			; fd 
+	literal recvfrom
+	os
+%endmacro
+		
+%macro send_socket 0
+	literal 0
+	arg4
+	arg3			; length tos
+	arg2			; buffer nos
+	arg1			; fd
+	literal send
+	os
+%endmacro
+
+%macro recv_socket 0
+	literal 0
+	arg4
+	arg3			; length tos
+	arg2			; buffer nos
+	arg1			; fd
+	literal recv
+	os
+%endmacro
+
+%macro write_socket 0
+	arg3			; length tos
+	arg2			; buffer nos
+	arg1			; fd
+	literal write
+	os
+%endmacro	
+
+%macro read_socket 0
+	arg3			; length tos
+	arg2			; buffer nos
+	arg1			; fd
+	literal read
+	os
+%endmacro
+
+%macro _shutdown 0
+	arg1 			; fd
+	literal shutdown
+	os
+%endmacro
+
+%macro shutdown_read_socket 0
+	literal 0
+	arg2
+	_shutdown
+%endmacro
+
+%macro shutdown_write_socket 0
+	literal 1
+	arg2
+	_shutdown
+%endmacro
+
+%macro shutdown_socket 0
+	literal 2
+	arg2
+	_shutdown
+%endmacro
